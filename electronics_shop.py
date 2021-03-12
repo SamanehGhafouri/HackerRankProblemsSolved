a7c83d57ecd37f08dfea6518d2f4ec6ad0043ca3
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-03-11 20:02:37 -0500

    Electronics shop

---
+++ electronics_shop.py
@@ -0,0 +1,21 @@
+# Electronics shop
+# A person wants to determine the most expensive computer
+# keyboard and USB drive that can be purchased with a give
+# budget. Given price lists for keyboards and USB drives and
+# a budget, find the cost to buy them. If it is not possible
+# to buy both items, return .
+
+
+def get_money_spent(keyboards, drives, b):
+    sum_of_items = [sum([i, j]) for i in keyboards for j in drives]
+    return max([i for i in sum_of_items if i <= b] + [-1])
+
+
+bnm = input().split()
+b = int(bnm[0])
+n = int(bnm[1])
+m = int(bnm[2])
+keyboards = list(map(int, input().rstrip().split()))
+drives = list(map(int, input().rstrip().split()))
+money_spent = get_money_spent(keyboards, drives, b)
+print(money_spent)
